7d788fb0c85fd2503c7bb8970aa133c252ffc9d7
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-12 19:52:41 +0530

    Modified postsController

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using aspCMS.Models;
 using aspCMS.Data;
+using System.ComponentModel;
 
 namespace aspCMS.Controllers;
 
@@ -15,8 +16,29 @@ public class PostsController : Controller
 
     public IActionResult Index()
     {
+
         List<Post> postsList = _db.Posts.ToList();
         return View(postsList);
+
+
+    }
+
+    public IActionResult Post(string? id)
+    {
+        Console.Write("cefefefe" + id);
+
+        if (id == null || id == "fail")
+        {
+
+            return View("Index");
+        }
+        else
+        {
+
+            Post postFound = _db.Posts.Where(post => post.Slug == id).FirstOrDefault();
+            return View(postFound);
+        }
+
     }
 
 
