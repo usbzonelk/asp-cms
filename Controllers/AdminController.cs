2be85352f79e1c2f02a091392e165e15ed4a2f6d
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-20 20:38:26 +0530

    Added Admin controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using aspCMS.Models;
+using aspCMS.Data;
+using HtmlAgilityPack;
+using aspCMS.Repository.UsersRepository;
+
+namespace aspCMS.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly IUsersRepository usersRepo;
+
+    public AdminController(IUsersRepository _userRepo)
+    {
+        usersRepo = _userRepo;
+    }
+
+    public IActionResult Index()
+
+    {
+
+        return View();
+
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
