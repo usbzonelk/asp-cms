0cbf43cfa849fd76fa8ca58289732951a54e7899
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-21 14:01:36 +0530

    Updated admin schema

--- Auth/AdminUsers.cs
+++ Auth/AdminUsers.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace aspCMS.Auth
 {
     public class AdminUsers : IdentityUser
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        [Required]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Password { get; set; }
     }
 }
