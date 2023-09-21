6fa78414dd847bc6c5e855cfe58d7e62d45a5903
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-21 23:29:24 +0530

    Added missed property in unitOFWork interface

--- Repository/IRepository/IUnitofwork.cs
+++ Repository/IRepository/IUnitofwork.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
+using aspCMS.Repository.CategoryRepository;
 using aspCMS.Repository.PostsRepository;
 
 namespace aspCMS.Repository;
 public interface IUnitOfWork
 {
     IPostsRepository Posts { get; }
-
+    ICategoryRepository Categories { get; }
     void Save();
 }
