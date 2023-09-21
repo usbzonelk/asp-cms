4b2f761dab99ba68b67380fb64de802d7496e4ba
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-21 23:26:46 +0530

    Integrated category repository with UnitOfWork

--- Repository/UnitOfWork.cs
+++ Repository/UnitOfWork.cs
@@ -4,17 +4,20 @@ using System.Linq.Expressions;
 using aspCMS.Repository.PostsRepository;
 using aspCMS.Data;
 using aspCMS.Models;
+using aspCMS.Repository.CategoryRepository;
 
 namespace aspCMS.Repository;
 public class UnitOfWork : IUnitOfWork
 {
     private AppDBContext _db;
     public IPostsRepository Posts { get; private set; }
+    public ICategoryRepository Categories { get; private set; }
 
     public UnitOfWork(AppDBContext db)
     {
         _db = db;
         Posts = new PostsRepository.PostsRepository(_db);
+        Categories = new CategoryRepository.CategoryRepository(_db);
     }
 
 
