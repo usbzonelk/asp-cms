80bac95b1ee8c9653f6a5a90c58c55bd7ea3177b
Bhashith Udawaththa <usbzonelk@gmail.com>
2023-09-21 23:14:22 +0530

    Added CategoryController

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using aspCMS.Models;
+using aspCMS.Data;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using aspCMS.Repository;
+
+namespace aspCMS.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public CategoryController(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
+    public IActionResult Index(string? id)
+    {
+        return RedirectToAction("Category", "Category");
+    }
+
+    public IActionResult Category(string? id)
+    {
+        if (id == null || id == "fail")
+        {
+            return View(null);
+        }
+        else
+        {
+
+            Category categoryFound = _unitOfWork.Category.GetPostBySlug(id);
+            return View(postFound);
+        }
+
+    }
+
+    
+}
